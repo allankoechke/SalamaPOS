8655cdd2da491b55c67e3591f2cb7423d06db43a
Allan K. Koech <allankoech.ke@gmail.com>
2024-08-15 17:29:05 +0300

    Update deploy script

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,4 +5,5 @@ cqtdeployer -confFile ./CQtDeployer.json
 # zip
 # cqt -bin Salama-POS -qmake /opt/Qt/5.15.2/gcc_64/bin/qmake -qmlDir ../../qml qif
 # cqt -bin Salama-POS -qmake /opt/Qt/5.15.2/gcc_64/bin/qmake -qmlDir ../../qml -icon icon.png -libDir /opt/Qt/5.15.2/gcc_64/lib qif
+# cqt -bin Salama-POS -qmake /opt/Qt/5.15.2/gcc_64/bin/qmake -qmlDir ../../qml -icon icon.png -libDir /opt/Qt/5.15.2/gcc_64/lib,/usr/lib/x86_64-linux-gnu qif
 
